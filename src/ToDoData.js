b8f3ea7aaf8ae139db762294ec4ee7be60de5ab5
Benedict-Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-02 04:10:32 +0100

    Adding list of items

---
+++ src/ToDoData.js
@@ -0,0 +1,27 @@
+const todoData = [{
+    id: 1,
+    text: "Take out the trash",
+    completed: true
+},
+{
+    id: 2,
+    text: "Grocery shopping",
+    completed: false
+},
+{
+    id: 3,
+    text: "Clean tank",
+    completed: false
+},
+{
+    id: 4,
+    text: "lawn the grass",
+    completed: true
+},
+{
+    id: 5,
+    text: "Catch up on your node pratical",
+    completed: false
+}]
+
+export default todoData
