f9dbe905ed82eefdff6dac512f1d7e97f16c2f3b
Benedict-Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-02 04:12:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
-import TodoItem from "./ToDoItem"
+import TodoItem from "./ToDoItem";
+import todoData from './ToDoData';
 
 function App() {
+  const todoItems = todoData.map(item => <TodoItem key={item.id} item={item}/>)
+  
   return (
     <div className = "todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+    {todoItems}
     </div>
   );
 }
