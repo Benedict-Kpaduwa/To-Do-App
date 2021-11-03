84bc7b82adafd2da13903f8e8c34c860a8eb73bc
Benedict-Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-03 17:46:16 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 //import logo from './logo.svg';
+import React from "react"
 import './App.css';
-import TodoItem from "./ToDoItem";
+import TodoItem from "./ToDoItem"
 import todoData from './ToDoData';
 
-class App extends React.Component {
+// function App() {
+//   const todoItems = todoData.map(item => <TodoItem key={item.id} item={item}/>)
+
+//   return (
+//     <div className = "todo-list">
+//       {todoItems}
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      todos = todoData
+      todos: todoData
     }
+    this.handleChange = this.handleChange.bind(this)
   }
-  const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+
+  handleChange(id){
+    this.setState(prevState=> {
+      const updatedTodo = prevState.todos.map(todo =>{
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodo
+      }
+    })
+  }
+
   render(){
-    return (
-    <div className = "todo-list">
-    {todoItems}
-    </div>
-  );
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+    return(
+      <div>
+        {todoItems}
+      </div>
+    );
   }
 }
 
