3dbaceef6132c885ca095bf71e80829e42fb3839
Benedict-Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2021-11-05 14:30:13 +0100

    Add files via upload
    Completed the styling

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 function ToDoItem(props){
+    const style = {
+        fontStyle: "italic",
+        color: "grey",
+        textDecoration: "line-through"
+
+    }
     return(
         <div className = "todo-item">
             <input type="checkbox" checked={props.item.completed} onChange={()=> props.handleChange(props.item.id)}/>
-            <p>{props.item.text}</p>
+            <p style={props.item.completed? style: null}>{props.item.text}</p>
         </div>
     );
 }
